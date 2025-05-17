c0932b81b0420aa8eba52fa2df1110251549d18f
Simple-hub-pro <abdujabborovmurodjon40@gmail.com>
2025-05-17 13:38:05 +0500

    bot MVP

---
+++ telegram_bot.py
@@ -0,0 +1,22 @@
+import telebot
+import os
+from dotenv import load_dotenv
+from functions import generate
+load_dotenv()
+
+bot = telebot.TeleBot(os.getenv("TELEGRAM_BOT_TOKEN"))
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, "Salom men SNRG AI botman" )
+
+@bot.message_handler(func=lambda message: True)
+def javob_qaytarish(message):
+    user_input = message.text
+    print("priyom!")
+    response = generate(user_input)
+    bot.send_message(message.chat.id, response)
+
+print("Bot ishga tushdi")
+bot.polling()
+    
